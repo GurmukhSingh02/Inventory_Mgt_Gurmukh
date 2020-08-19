04c3f0a34216acaee02b69a6ddb9f39e158e1b0d
Arshdeep Kaur <AK046@atcltd.atc.org.nz>
2020-08-19 16:53:34 +1000

    Add project files.

---
+++ Inventory_Mgt_Gurmukh/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory_Mgt_Gurmukh.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+
+        public List<StockMaintain> StockMaintains { get; set; }
+    }
+}
